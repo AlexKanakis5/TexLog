333d1b35fd7bd666bc76f3f29193d9f768c09f2c
Petros Panou <45739005+ppeter7@users.noreply.github.com>
2021-06-01 16:55:27 +0300

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,23 @@
+public class Player {
+    public boolean acceptance;
+    public boolean understanding;
+    
+    public Player(){
+        acceptance=false;
+        understanding=false;
+    }
+    
+    public void acceptCards() {
+        acceptance=true;
+    }
+    
+    public void understandCards() {
+        understanding=true;
+    }
+    
+    public void printState() {
+        System.out.println("acceptancce="+acceptance);
+        System.out.println("understanding="+understanding);
+    }
+}
+
