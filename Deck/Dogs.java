dcbed7ee5357c0fcfcd247796d640395db7e2c07
Kostas Koutiklias <76959628+koskou21@users.noreply.github.com>
2021-06-05 19:24:51 +0300

    Update Dogs.java

--- Deck/Dogs.java
+++ Deck/Dogs.java
@@ -3,8 +3,8 @@ public class Dogs extends SpecialCards {
     //constructor
     public Dogs(){
         super();
-        this.setArithmos(0);
         this.setPontoi(0);
+        this.setArithmos(0);
         this.color(NONE);
     }
 }
