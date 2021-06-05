abe5cfafa4f189cdd83c4c15e4947d139f4d55ea
Kostas Koutiklias <76959628+koskou21@users.noreply.github.com>
2021-06-05 18:15:58 +0300

    Update CommonCards.java

--- Deck/CommonCards.java
+++ Deck/CommonCards.java
@@ -1,1 +1,53 @@
+public class CommonCards extends Cards {
 
+  private int rank;
+
+	private static String[] ranks  = { "-1","-1" , "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13","14" };
+
+
+	public CommonCards(Color color, int rank)
+	{
+		this.rank=rank;
+                this.setPontoi(rank);
+		this.color=color;
+                this.setArithmos(0);
+                
+                if(ranks[rank].equals("5")){
+                    this.setPontoi(5);
+                    this.setArithmos(5);
+                }
+                else if(ranks[rank].equals("10")){
+                    this.setPontoi(10);
+                    this.setArithmos(10);
+                }
+                else if(ranks[rank].equals("11")){
+                    this.setPontoi(11);
+                    this.setArithmos(0);
+                }
+                else if(ranks[rank].equals("12")){
+                    this.setPontoi(12);
+                    this.setArithmos(0);
+                }
+                else if(ranks[rank].equals("13")){
+                    this.setPontoi(13);
+                    this.setArithmos(10);
+                }
+                else if(ranks[rank].equals("14")){
+                    this.setPontoi(14);
+                    this.setArithmos(0);
+                }
+                
+	}
+
+  
+    //epistrefoume to rank
+    
+    public String getRank() {
+        return ranks[rank];
+    }
+
+    // kanoume rank to rank sto set
+    public void setRank(int rank) {
+        this.rank = rank;
+    }    
+}
