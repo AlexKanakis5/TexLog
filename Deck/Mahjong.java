596dac81bef1b6581e6459e0c073914c3ef0c006
Kostas Koutiklias <76959628+koskou21@users.noreply.github.com>
2021-06-05 19:28:36 +0300

    Update Mahjong.java

--- Deck/Mahjong.java
+++ Deck/Mahjong.java
@@ -1,1 +1,27 @@
+public class Mahjong extends SpecialCards{
+    int DemandNumber;
+  
+    //constructor
+  
+  public Mahjong()
+    {
+        super();
+        this.setArithmos(1);
+        this.setPontoi(0);
+        this.color(NONE);
+    }
 
+  // Epistrefei ton arithmo pou zhthse o paikths
+  
+    public int getDemandNumber() {
+        return DemandNumber;
+    }
+
+    /**
+     * transformers
+     * thetei sto DemandNumber ton arithmo pou zitithike
+     */
+    public void setDemandNumber(int DemandNumber) {
+        this.DemandNumber = DemandNumber;
+    }
+}
