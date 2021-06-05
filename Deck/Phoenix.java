c9b7275da150f748dfe782a735f75abfdb9858d1
Kostas Koutiklias <76959628+koskou21@users.noreply.github.com>
2021-06-05 19:11:56 +0300

    Update Phoenix.java

--- Deck/Phoenix.java
+++ Deck/Phoenix.java
@@ -1,11 +1,8 @@
 public class Phoenix extends SpecialCards{
 
+     //constructor
+     //h monh karta sto paixnidi tupou Foinika. Mporei na exei 2 xrhseis: a) se monofullia  b) san mpalanter se zeugari
     
-    /**
-     * constructor
-     * h monh karta sto paixnidi tupou Foinika
-     * Mporei na exei 2 xrhseis: a) se monofullia  b) san mpalanter se zeugari
-     */
     public Phoenix(){
         super();
         this.setCredits(-25);
