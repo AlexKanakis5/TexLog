e2b73fa5bddd5bbaeaa7237497ec30c726239ef2
Kostas Koutiklias <76959628+koskou21@users.noreply.github.com>
2021-06-05 19:10:20 +0300

    Update Drake.java

--- Deck/Drake.java
+++ Deck/Drake.java
@@ -1,1 +1,10 @@
-
+public class Drake extends SpecialCards{
+    
+    
+    public Drake(){
+        super();
+        this.setCredits(25);
+        this.setValue(20);
+        this.color(NONE);    
+    }
+}
