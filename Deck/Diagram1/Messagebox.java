f65d679482f27609b12b923defde6c9dcf18f20a
Andreaseco <85455537+Andreaseco@users.noreply.github.com>
2021-06-08 19:58:14 +0300

    Create Messagebox.java

---
+++ Deck/Diagram1/Messagebox.java
@@ -0,0 +1,29 @@
+public class Messagebox
+{
+    // i sinartisi ektipwnei to minima epitiximenis apostolis | liyis
+    public void print_messagebox(int x)
+    {
+        // gia x = 0 ektipwnei to minima 1
+        // oti o paixtis esteile ta filla kai perimenei
+        // gia x = 1 ektipwnei to minima 2
+        // oti o paixtis prepei na apodexti ta filla pou tou stelnoun oi alloi
+        if(x == 0)
+        {
+            System.out.println("You are waiting other players finish the trade\n");
+        }
+        else
+        {
+            System.out.println("Other player are waiting you to take your cards\n");
+        }
+    }
+    //i sinartisi tha prepei na ektipwnei minima apodoxis ston xristi
+     public void press_done()
+    {
+        
+    }
+    //ligo prin tin lixi tou xronou antalagis kalite to minima proidopoiseis
+    public void print_warning()
+    {
+        System.out.println("You have to finish trading hurry up!\n");
+    }
+}
