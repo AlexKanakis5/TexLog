b16c30cfd6f5879b10f230d6e3405f8979af0025
Andreaseco <85455537+Andreaseco@users.noreply.github.com>
2021-06-08 18:43:15 +0300

    Add files via upload

---
+++ Deck/Diagram1/Tournament.java
@@ -0,0 +1,72 @@
+// i sigkekrimeni klasi tha ftiaxnei oles tis sunartisis tou 
+// tournoua
+public class Tournament
+{
+    
+    // pernei san orisma twn pinaka ton omadwn
+    // kai tous xwrizei se trapezia
+    public void start_game()
+    {
+       
+    }
+    // enarxei tis faseis 6
+    public void phase_six_start()
+    {
+       
+    }
+    // enarxei tis faseis 5
+    public void phase_five_start()
+    {
+       
+    }
+    // enarxei tis faseis 4
+    public void phase_four_start()
+    {
+       
+    }
+    // enarxei tis faseis 3
+    public void phase_three_start()
+    {
+       
+    }
+    // enarxei tis faseis 2
+    public void phase_two_start()
+    {
+       
+    }
+    // enarxei tis faseis 1
+    public void phase_one_start()
+    {
+       
+    }
+     // lixi tis faseis 6
+    public void phase_six_end()
+    {
+       
+    }
+     // lixi tis faseis 5
+    public void phase_five_end()
+    {
+       
+    }
+     // lixi tis faseis 4
+    public void phase_four_end()
+    {
+       
+    }
+     // lixi tis faseis 3
+    public void phase_three_end()
+    {
+       
+    }
+     // lixi tis faseis 2
+    public void phase_two_end()
+    {
+       
+    }
+     // lixi tis faseis 1
+    public void phase_one_end()
+    {
+       
+    }
+}
